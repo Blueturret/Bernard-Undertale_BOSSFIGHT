21c2660b51db8c62c0b986dab95d1a40c2fa37a9
Blueturret <patrick06.afram@gmail.com>
2025-01-12 21:29:13 +0100

    Added Player Movement
    You can now mooooooove!

---
+++ Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterControls : MonoBehaviour
+{
+    Rigidbody2D rb;
+    PlayerInput playerInput;
+
+    float horizontalInput, verticalInput;
+    [SerializeField] float speed; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2 (horizontalInput, verticalInput).normalized * speed * Time.deltaTime;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalInput = context.ReadValue<Vector2>().x;
+        verticalInput = context.ReadValue<Vector2>().y;
+    }
+}
